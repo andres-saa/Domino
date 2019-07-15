ff527ab04f0d535ccf2a6b8a23505d8fb7537365
Andres <andres.arrechea@correounivalle.edu.co>
2019-07-15 15:14:37 -0500

    veamos

--- src/domino/Ficha.java
+++ src/domino/Ficha.java
@@ -6,5 +6,15 @@ public class Ficha {
 	public Ficha(int lado1,int lado2) {
 		this.lado1=lado1;
 		this.lado2=lado2;
-	};
+	}
+
+	public int getLado1() {
+		return lado1;
+	}
+
+	public int getLado2() {
+		return lado2;
+	}
+	
+	
 }
