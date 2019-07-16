5155212c570cf5d64082a4c3b0ec3ab899815122
Andres <andres.arrechea@correounivalle.edu.co>
2019-07-16 09:53:57 -0500

    nueva ficha

--- src/domino/Domino.java
+++ src/domino/Domino.java
@@ -7,6 +7,7 @@ import java.awt.GridBagLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -17,67 +18,68 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Domino extends JFrame {
-	
-	
-	Baraja fichas= new Baraja();
-	ArrayList<Ficha> juego1=new ArrayList<Ficha>();
-	ArrayList<Ficha> juego2=new ArrayList<Ficha>();
+
+	Baraja fichas = new Baraja();
+	ArrayList<Ficha> juego1 = new ArrayList<Ficha>();
+	ArrayList<Ficha> juego2 = new ArrayList<Ficha>();
 	Jugador jugador1 = new Jugador(juego1);
 	Jugador jugador2 = new Jugador(juego2);
-	ArrayList<JButton> mesa = new ArrayList<JButton>();
-	JPanel panel = new JPanel(); 
-	
+	Ficha primeraFichaJugador1=new Ficha(0,0,new ImageIcon());
+	Ficha primeraFichaJugador2=new Ficha(0,0,new ImageIcon());
+	Random PrimeraFicha=new Random();
 
-	//private BufferedImage bufferFondo = null;
-	//private JLabel centralLabel;
-	
+	ArrayList<JButton> mesa = new ArrayList<JButton>();
+	JPanel panel = new JPanel();
+
+	// private BufferedImage bufferFondo = null;
+	// private JLabel centralLabel;
+
 	public Domino() {
 		try {
-			
-		//	fichas.repartir(jugador1);
-		//	fichas.repartir(jugador2);
-			
+
+			// fichas.repartir(jugador1);
+			// fichas.repartir(jugador2);
+
 			initGUI();
-			
-			//Default window config.
+
+			// Default window config.
 			this.setUndecorated(false);
 			pack();
 			this.setResizable(false);
 			this.setLocationRelativeTo(null);
 			this.setVisible(true);
-			this.setSize(1280,720);		}
-		
+			this.setSize(1280, 720);
+		}
+
 		catch (Exception e) {
-			//TODO: handle expection
+			// TODO: handle expection
 			JOptionPane.showMessageDialog(null, "No se ha encontrado la imagen de fondo");
 		}
 	}
-	
+
 	private void initGUI() {
-		JLabel imagenDeFondo=new JLabel(new ImageIcon("src/imagenes/fondo.jpg"));
+		JLabel imagenDeFondo = new JLabel(new ImageIcon("src/imagenes/fondo.jpg"));
 		crearMesa();
-		
+		//quienEmpieza();
 		this.getContentPane().add(panel);
 		panel.setLayout(null);
-		imagenDeFondo.setBounds(0,0,1280,720);
+		imagenDeFondo.setBounds(0, 0, 1280, 720);
 		panel.add(imagenDeFondo);
-		
-		for (int i=0;i<28;i++) {
-		imagenDeFondo.add(mesa.get(i));
+
+		for (int i = 0; i < 28; i++) {
+			imagenDeFondo.add(mesa.get(i));
 		}
-		
+
 	}
 
-	
 	public void crearMesa() {
-
 
 		// para que las fichas no se repitan ya que ficha(a,b)=ficha(b,a)
 		int contador = 0;
 		// vamos a crear los labels de las fichas y las agrega al array de la mesa.
 		for (int lado1 = 0; lado1 < 7; lado1++) {
 			for (int lado2 = contador; lado2 < 7; lado2++) {
-				mesa.add(new JButton(new ImageIcon("src/fichas/"+lado1+""+lado2+".png")));
+				mesa.add(new JButton(new ImageIcon("src/fichas/" + lado1 + "" + lado2 + ".png")));
 			}
 			contador++;
 		}
@@ -87,9 +89,9 @@ public class Domino extends JFrame {
 	public void establecerPosicionAlasFichas() {
 		int posicionInicialx = 200;
 		int posicionInicialy = 180;
-		int permutador=0;
+		int permutador = 0;
 		for (int i = 0; i < 4; i++) {
-			for (int cual = permutador; cual < permutador+7; cual++) {
+			for (int cual = permutador; cual < permutador + 7; cual++) {
 				mesa.get(cual).setBounds(posicionInicialx, posicionInicialy, 43, 85);
 				// mesa.get(cual).setBorderPainted(false);
 				// mesa.get(cual).setContentAreaFilled(false);
@@ -97,12 +99,19 @@ public class Domino extends JFrame {
 				// mesa.get(cual).setOpaque(true);
 				posicionInicialx += 48;
 			}
-			posicionInicialx=200;
-			permutador+=7;
+			posicionInicialx = 200;
+			permutador += 7;
 			posicionInicialy += 89;
 		}
 
 	}
+
+	public void quienEmpieza() {
+		
+		//primeraFichajugador1=
+		
+	};
 }
+
 
 
